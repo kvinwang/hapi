1d96c9af2fa457ffc7bb42cb137808f20935fe84
Kevin Wang <wy721@qq.com>
2026-03-13 11:29:43 +0000

    Use corner-left-up icon for parent directory button

--- web/src/routes/sessions/files.tsx
+++ web/src/routes/sessions/files.tsx
@@ -315,7 +315,7 @@ function CwdBar(props: {
                 className="shrink-0 text-[var(--app-hint)] hover:text-[var(--app-fg)] transition-colors"
                 title="Go to parent directory"
             >
-                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="18 15 12 9 6 15" /></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M14 9 9 4 4 9" /><path d="M20 20h-7a4 4 0 0 1-4-4V4" /></svg>
             </button>
             <button
                 type="button"
